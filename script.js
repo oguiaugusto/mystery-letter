65bdd3c2882931f1a74a7970f0da21c3204ec777
gui <guilhaugusto05@gmail.com>
2021-09-14 16:15:24 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -3,18 +3,29 @@ const generatedLetter = document.getElementById('carta-gerada');
 const input = document.getElementById('carta-texto');
 const button = document.getElementById('criar-carta');
 
-function generateLetter() {
+function removeOldLetter() {
   while (generatedLetter.firstChild) {
     const toRemove = generatedLetter.firstChild;
     generatedLetter.removeChild(toRemove);
   }
-  const letter = input.value;
-  const letterWords = `${letter}`.split(' ');
-  for (let i = 0; i < letterWords.length; i += 1) {
+}
+
+function generateLetter() {
+  if (input.value === '' || input.value === ' ' || `${input.value}`.includes('  ')) {
     const word = document.createElement('span');
-    word.innerText = letterWords[i];
-    word.className = 'word';
+    const letter = 'Por favor, digite o conteúdo da carta.';
+    word.innerText = letter;
     generatedLetter.appendChild(word);
+  } else {
+    removeOldLetter();
+    const letter = input.value;
+    const letterWords = `${letter}`.split(' ');
+    for (let i = 0; i < letterWords.length; i += 1) {
+      const word = document.createElement('span');
+      word.innerText = letterWords[i];
+      word.className = 'word';
+      generatedLetter.appendChild(word);
+    }
   }
 }
 
